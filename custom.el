2dfb0e52bee2ed5cf896c356a545f648b1b49116
Dmitriy Kropivnitskiy <nigde@mitechki.net>
2015-07-27 15:12:47 -0600

    Add customized settings

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elpy-default-minor-modes (quote (eldoc-mode flymake-mode yas-minor-mode auto-complete-mode)))
+ '(erc-modules (quote (autojoin button completion dcc fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring services stamp track)))
+ '(explicit-shell-file-name "/bin/bash")
+ '(hl-paren-colors (quote ("red" "orange" "lightblue1" "green1" "purple" "blue")))
+ '(javascript-indent-level 2)
+ '(js-indent-level 2)
+ '(js2-basic-offset 2)
+ '(multi-term-switch-after-close nil)
+ '(python-indent-guess-indent-offset nil)
+ '(term-bind-key-alist (quote (("C-c C-c" . term-interrupt-subjob) ("C-p" . previous-line) ("C-n" . next-line) ("C-s" . isearch-forward) ("C-r" . term-send-raw) ("C-m" . term-send-raw) ("M-f" . term-send-forward-word) ("M-b" . term-send-backward-word) ("M-o" . term-send-backspace) ("M-p" . term-send-up) ("M-n" . term-send-down) ("M-M" . term-send-forward-kill-word) ("M-N" . term-send-backward-kill-word) ("M-r" . term-send-reverse-search-history) ("M-," . term-send-input) ("M-." . comint-dynamic-complete))))
+ '(term-unbind-key-list (quote ("C-z" "C-x" "C-c" "C-h" "C-y" "C-r" "<ESC>")))
+ '(yaml-indent-offset 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Monospace")))))
